be404a88b21e29d70c263017232b7f821b65ebd8
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-09 06:33:37 +0700

    Setup basic style and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
-  return <div></div>;
+  return (
+    <main className="min-h-screen w-full flex-center flex-col">
+      <h1 className="text-center">Cek Sertifikat</h1>
+      <p className="text-center text-gray-400">
+        Cek keaslian sertifikat webinar Profesi Keuangan Expo
+      </p>
+
+      <form>
+        <label htmlFor="certificateCode">Nomor Sertifikat</label>
+        <input id="certificateCode" name="certificateCode" type="text" />
+
+        <button type="submit">Cek Sertifikat</button>
+      </form>
+    </main>
+  );
 }
 
 export default App;
