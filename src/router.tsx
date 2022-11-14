10210e60e628b16a4e104075ddeb32e65ea94d15
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-14 16:14:12 +0700

    Register admin page

--- src/router.tsx
+++ src/router.tsx
@@ -3,8 +3,12 @@ import { createBrowserRouter, Route } from "react-router-dom";
 
 // Components
 import App from "./App";
+import Admin from "./routes/Admin";
 
-const routes = [{ path: "/", element: <App /> }];
+const routes = [
+  { path: "/", element: <App /> },
+  { path: "/admin", element: <Admin /> },
+];
 
 const router = createBrowserRouter(routes);
 export default router;
