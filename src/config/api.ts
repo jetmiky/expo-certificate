f815f1c6962daf5163dab1dae5c1112324d9392c
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-15 13:53:14 +0700

    Setup basic API

---
+++ src/config/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const baseURL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:5001/expo-certificate/us-central1/api";
+
+const api = axios.create({ baseURL });
+export default api;
