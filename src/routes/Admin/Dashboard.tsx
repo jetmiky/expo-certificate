cc3ec9294dcaad69865a7903a22864f2ba53bd0b
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-16 11:17:39 +0700

    Basic dashboard structure

--- src/routes/Admin/Dashboard.tsx
+++ src/routes/Admin/Dashboard.tsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
+// Components
+import Button from "../../components/Button";
+
 export default function AdminDashboard(): JSX.Element {
-  return <h1>Admin Dashboard here!</h1>;
+  const [isModalAddShown, setIsModalAddShown] = useState(false);
+  const [isModalBatchAddShown, setIsModalBatchAddShown] = useState(false);
+  const [isModalSearchShown, setIsModalSearchShown] = useState(false);
+
+  const handleToggleModal = (modal: "add" | "batch" | "search") => () => {
+    if (modal === "add") return setIsModalAddShown(!isModalAddShown);
+    if (modal === "batch")
+      return setIsModalBatchAddShown(!isModalBatchAddShown);
+    if (modal === "search") return setIsModalSearchShown(!isModalSearchShown);
+  };
+
+  return (
+    <section>
+      <h2>Admin Dashboard here!</h2>
+      <p>What do you want to do?</p>
+
+      <Button onClick={handleToggleModal("add")}>Add certificate</Button>
+      <Button onClick={handleToggleModal("batch")}>
+        Batch add certificate
+      </Button>
+      <Button onClick={handleToggleModal("search")}>Search certificate</Button>
+    </section>
+  );
 }
