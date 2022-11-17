466e0f7ad5c98596f79979915330af705cec1938
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-17 16:46:35 +0700

    Refactor code

--- src/routes/Home/Modals/ModalResult.tsx
+++ src/routes/Home/Modals/ModalResult.tsx
@@ -22,19 +22,19 @@ export default function ModalResult({
         <>
           <Alert type="primary">
             <svg
-              className="checkmark inline w-8 h-8 mr-3"
+              className="mark check inline w-8 h-8 mr-3"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 52 52"
             >
               <circle
-                className="checkmark__circle"
+                className="mark__circle check"
                 cx="26"
                 cy="26"
                 r="25"
                 fill="none"
               />
               <path
-                className="checkmark__check"
+                className="mark__path"
                 fill="none"
                 d="M14.1 27.2l7.1 7.2 16.7-16.8"
               />
@@ -79,24 +79,24 @@ export default function ModalResult({
         <>
           <Alert type="secondary">
             <svg
-              className="crossmark inline w-8 h-8 mr-3"
+              className="mark cross inline w-8 h-8 mr-3"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 52 52"
             >
               <circle
-                className="crossmark__circle"
+                className="mark__circle cross"
                 cx="26"
                 cy="26"
                 r="25"
                 fill="none"
               />
               <path
-                className="cross__path cross__path--right"
+                className="mark__path cross__path--right"
                 fill="none"
                 d="M16,16 l20,20"
               />
               <path
-                className="cross__path cross__path--left"
+                className="mark__path cross__path--left"
                 fill="none"
                 d="M16,36 l20,-20"
               />
