8e403cc847f52785f0d9b8e47204a93c3c459795
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-17 16:17:48 +0700

    Add crossmark styling on modal result

--- src/routes/Home/Modals/ModalResult.tsx
+++ src/routes/Home/Modals/ModalResult.tsx
@@ -77,7 +77,32 @@ export default function ModalResult({
         </>
       ) : (
         <>
-          <Alert type="secondary">Sertifikat tidak terdaftar</Alert>
+          <Alert type="secondary">
+            <svg
+              className="crossmark inline w-8 h-8 mr-3"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 52 52"
+            >
+              <circle
+                className="crossmark__circle"
+                cx="26"
+                cy="26"
+                r="25"
+                fill="none"
+              />
+              <path
+                className="cross__path cross__path--right"
+                fill="none"
+                d="M16,16 l20,20"
+              />
+              <path
+                className="cross__path cross__path--left"
+                fill="none"
+                d="M16,36 l20,-20"
+              />
+            </svg>
+            Sertifikat tidak terdaftar
+          </Alert>
           <p className="mt-2">
             Sertifikat tidak terdaftar di basis data Profesi Keuangan Expo.
           </p>
