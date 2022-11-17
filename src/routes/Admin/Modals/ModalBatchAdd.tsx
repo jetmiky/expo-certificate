cb4a1ae43b635b82ae9fb9b25cc552579cb66226
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-17 12:19:57 +0700

    Add basic structure

--- src/routes/Admin/Modals/ModalBatchAdd.tsx
+++ src/routes/Admin/Modals/ModalBatchAdd.tsx
@@ -1,5 +1,9 @@
+import { FormEvent } from "react";
+
 // Components
 import Modal from "../../../components/Modal";
+import Input from "../../../components/Input";
+import Button from "../../../components/Button";
 
 interface Props {
   onToggle: Function;
@@ -8,9 +12,19 @@ interface Props {
 export default function ModalBatchAdd(props: Props): JSX.Element {
   const { onToggle } = props;
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Submitted");
+  };
+
   return (
     <Modal title="Batch Add Certificate" onToggle={onToggle}>
-      <h3>Modal Batch Add Certificate</h3>
+      <form onSubmit={handleSubmit}>
+        <Input type="file" id="file" label="File" name="file" required />
+
+        <Button>Submit</Button>
+      </form>
     </Modal>
   );
 }
