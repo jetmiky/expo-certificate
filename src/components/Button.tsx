d9f1c4b031b05fbaab9a9b55c1a8ceb254a78b96
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-17 09:27:55 +0700

    Add theme and HTMLButton props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,32 +1,50 @@
-import { ReactNode } from "react";
+import { DetailedHTMLProps, ButtonHTMLAttributes, ReactNode } from "react";
 
-interface Props {
-  type?: "primary" | "secondary";
+interface Props
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  theme?: "indigo" | "green" | "rose";
   isLoading?: boolean;
-  disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
 }
 
-export default function Button({
-  type = "primary",
-  isLoading = false,
-  disabled = false,
-  children,
-  onClick,
-}: Props): JSX.Element {
-  const classes =
-    type === "primary"
-      ? "bg-indigo-500 shadow-indigo-200 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-indigo-600 disabled:bg-indigo-400/80"
-      : type === "secondary"
-      ? "bg-green-500 shadow-green-200 hover:bg-green-600 focus:bg-green-600 focus:ring-green-600 disabled:bg-green-400/80"
-      : "";
+export default function Button(props: Props): JSX.Element {
+  const {
+    theme = "indigo",
+    isLoading = false,
+    disabled = false,
+    children,
+    onClick,
+    ...rest
+  } = props;
+
+  let classes: string;
+
+  switch (theme) {
+    case "indigo":
+      classes =
+        "bg-indigo-500 shadow-indigo-200 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-indigo-600 disabled:bg-indigo-400/80";
+      break;
+    case "green":
+      classes =
+        "bg-green-500 shadow-green-200 hover:bg-green-600 focus:bg-green-600 focus:ring-green-600 disabled:bg-green-400/80";
+      break;
+    case "rose":
+      classes =
+        "bg-rose-500 shadow-rose-200 hover:bg-rose-600 focus:bg-rose-600 focus:ring-rose-600 disabled:bg-rose-400/80";
+    default:
+      classes = "";
+  }
 
   return (
     <button
-      className={`${classes} px-4 py-2 outline-none rounded text-white shadow-lg font-medium active:shadow-none active:scale-95  focus:ring-2 focus:ring-offset-2 disabled:shadow-none disabled:cursor-not-allowed transition-all transition-ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0`}
+      className={`${classes} px-4 py-2 outline-none rounded text-white shadow-lg font-medium active:shadow-none active:scale-95 focus:ring-2 focus:ring-offset-2 disabled:shadow-none disabled:cursor-not-allowed transition-all transition-ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0`}
       onClick={onClick}
       disabled={disabled || isLoading}
+      {...rest}
     >
       {isLoading ? (
         <>
