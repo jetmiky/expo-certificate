f608893f1e6afc89a1459a5141918df24b3352b5
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-17 09:44:38 +0700

    Admin page container styling

--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -1,12 +1,21 @@
+// Router
 import { Outlet } from "react-router-dom";
+
+// Components
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 export default function Admin() {
   return (
-    <div>
-      <h1>This is Admin Page</h1>
+    <div className="min-h-screen w-full flex items-center justify-between flex-col">
+      <header>
+        <Header />
+        <h1 className="text-center text-xl mt-3">ADMINISTRATOR</h1>
+      </header>
 
-      <p>Outlet goes below.</p>
       <Outlet />
+
+      <Footer />
     </div>
   );
 }
