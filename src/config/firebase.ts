fc764f74f083ddc75b3ce27bf52604a4c6d0a3aa
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-21 14:50:42 +0700

    Add firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmgkb5oToFpFHn48F206oJ-a8hqHfj1nk",
+  authDomain: "expo-certificate.firebaseapp.com",
+  projectId: "expo-certificate",
+  storageBucket: "expo-certificate.appspot.com",
+  messagingSenderId: "517458300355",
+  appId: "1:517458300355:web:b0ba2b7927884c6bfc0176",
+  measurementId: "G-9K2Y38TDKL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+if (import.meta.env.MODE === "production") {
+  connectAuthEmulator(auth, "http://localhost:9099");
+}
