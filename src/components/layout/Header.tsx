7b49d9660b197919ef51bfae6a87943d417576af
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-25 14:43:09 +0700

    Fixed logo on mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,11 @@ export default function Header(): JSX.Element {
       <div className="grid grid-cols-4 gap-2 md:grid-cols-6 ">
         <div className="col-start-2 col-span-2 md:col-start-3 flex justify-center h-20">
           <a href="https://profesikeuanganexpo.id" target="_blank">
-            <img src={logo} className="h-full" alt="Profesi Keuangan Expo" />
+            <img
+              src={logo}
+              className="max-h-full"
+              alt="Profesi Keuangan Expo"
+            />
           </a>
         </div>
       </div>
