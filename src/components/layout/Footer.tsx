366ab48ae5d9ed9798f91f6d9e072d4df09ac38f
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-25 14:39:30 +0700

    Footer view on mobile

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,18 +6,20 @@ import SocialLink from "../SocialLink";
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="container py-4 flex justify-between">
-      <p className="text-sm text-gray-500">
-        &copy; 2022 All rights reserved - Pusat Pembinaan Profesi Keuangan
-      </p>
+    <footer className="container py-4">
+      <div className="flex flex-col items-center md:justify-between md:flex-row">
+        <p className="text-sm text-gray-500 text-center md:text-left">
+          &copy; 2022 All rights reserved - Pusat Pembinaan Profesi Keuangan
+        </p>
 
-      <div className="flex mt-4 space-x-3 sm:justify-center sm:mt-0">
-        <SocialLink url={social.youtube} icon="youtube" hoverColor="red" />
-        <SocialLink
-          url={social.instagram}
-          icon="instagram"
-          hoverColor="indigo"
-        />
+        <div className="flex mt-4 space-x-3 sm:justify-center sm:mt-0">
+          <SocialLink url={social.youtube} icon="youtube" hoverColor="red" />
+          <SocialLink
+            url={social.instagram}
+            icon="instagram"
+            hoverColor="indigo"
+          />
+        </div>
       </div>
     </footer>
   );
