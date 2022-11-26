665c11e6d3d7bfca3db2ba5ea9ef1d8fabb9028b
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-26 12:24:23 +0700

    Resize logo image

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export default function Header(): JSX.Element {
   return (
     <header className="pt-5 container">
       <div className="grid grid-cols-4 gap-2 md:grid-cols-6 ">
-        <div className="col-start-2 col-span-2 md:col-start-3 flex justify-center h-20">
+        <div className="col-start-2 col-span-2 md:col-start-3 flex justify-center md:h-20 2xl:h-40">
           <a href="https://profesikeuanganexpo.id" target="_blank">
             <img
               src={logo}
