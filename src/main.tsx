ed398f3880180f21aaeffd51d54370519af3caa9
Bima Jatmiko Abadi <bja.devs@gmail.com>
2022-11-26 12:10:46 +0700

    Move auth provider to admin route

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-// Contexts
-import AuthProvider from "./context/auth/AuthProvider";
-
 import App from "./App";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -6,35 +6,40 @@ import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import PrivateRoute from "../components/PrivateRoute";
 
+// Contexts
+import AuthProvider from "../context/auth/AuthProvider";
+
 // Pages
 import Login from "./Admin/Login";
 import Dashboard from "./Admin/Dashboard";
 
 export default function Admin() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-between flex-col">
-      <header>
-        <Header />
-        <h1 className="text-center text-xl mt-3">ADMINISTRATOR</h1>
-      </header>
+    <AuthProvider>
+      <div className="min-h-screen w-full flex items-center justify-between flex-col">
+        <header>
+          <Header />
+          <h1 className="text-center text-xl mt-3">ADMINISTRATOR</h1>
+        </header>
 
-      <Routes>
-        <Route index element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
 
-      <Outlet />
+        <Outlet />
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
